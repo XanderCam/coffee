4db4865990ad22abb55f2c5615566dd4693821b9
CodeSandbox <support@codesandbox.io>
2025-04-01 06:51:46 +0000

    Fix Android SDK configuration for CI

--- app/src/main/java/com/example/coffeecounter/spotify/SpotifyService.kt
+++ app/src/main/java/com/example/coffeecounter/spotify/SpotifyService.kt
@@ -4,6 +4,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
+import retrofit2.Retrofit
 
 interface SpotifyApiService {
     @GET("v1/recommendations")
