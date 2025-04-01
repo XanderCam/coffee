813901ae6b4c893ef88f89185d36b3cf391ebf81
CodeSandbox <support@codesandbox.io>
2025-04-01 11:08:26 +0000

    Added Spotify SDK integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-configurations.all {
-    exclude(group = "com.android.support")
-}
-
 android {
     namespace = "com.example.coffeecounter"
     compileSdk = 34
@@ -20,8 +16,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -47,40 +41,23 @@ android {
 
 dependencies {
     implementation(project(":feature-phone"))
-    // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
-    
-    // UI Components
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    
-    // Architecture Components
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    
-    // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
-
-    // Core AndroidX
-    implementation("androidx.annotation:annotation:1.7.1")
-    
-    // Room Database
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
-
+    
     // Web API
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.spotify.android:auth:2.1.1") // Spotify SDK
 
-    // Testing (minimal)
     testImplementation("junit:junit:4.13.2")
 }
